eff9c83a9571945d0a7b4cd3adffa458a3b90c54
Mohan <Krishnanmohan49@yahoo.com>
2021-09-27 14:37:50 +0530

    WebdriverIO with TypeScript - change JSON data based on file path

---
+++ src/pages/waitex.page.ts
@@ -0,0 +1,12 @@
+class waitEx{
+
+    get startbtn() {return $('#start button')}
+    get loadingIco() {return $('#loading')}
+    get msg() {return $('#finish h4')}
+
+
+
+}
+
+
+export default new waitEx()
--- test/steps/waitexsteps.ts
+++ test/steps/waitexsteps.ts
@@ -1,5 +1,5 @@
 import { Given, When, Then } from '@cucumber/cucumber'
-//import waitexpage from "src/pages/waitex.page"
+import waitexpage from "src/pages/waitex.page"
 
 Given(/^I am on dynamic loading (.+) page$/, async (appurl: string) => {
     await browser.maximizeWindow()
@@ -8,19 +8,18 @@ Given(/^I am on dynamic loading (.+) page$/, async (appurl: string) => {
 
 When(/^I click on start button$/, async () => {
 
-    const startbtn =$('#start button')
-    await( await startbtn).click()
+   
+    await waitexpage.startbtn.click()
     
 });
 
 Then(/^I validate loading icon$/, async () => {
 
-    const loadingIco =$('#loading')
-    const msg =$('#finish h4')
-    await (await loadingIco).waitForDisplayed({timeout:5000})
-    await (await loadingIco).waitForDisplayed({reverse:true,timeout:5000}) // this will wait for the element to disappaear
+   
+    await waitexpage.loadingIco .waitForDisplayed({timeout:5000})
+    await waitexpage.loadingIco.waitForDisplayed({reverse:true,timeout:5000}) // this will wait for the element to disappaear
   //  await (await msg).waitForDisplayed({timeout:10000})
-    await browser.waitUntil(async()=>await(await msg).getText()==='Hello world!',{ 
+    await browser.waitUntil(async()=>await waitexpage.msg.getText()==='Hello world!',{ 
         timeout:120000,
         timeoutMsg:"element is not displayed in 10 sec"
     })
